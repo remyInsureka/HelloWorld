ee1dc312dcaecaf94b33e75f26df3843b233cdf0
Ramesh R <ramesh@insureka.com>
2023-09-16 16:17:58 +0530

    first commit

--- HelloWorldProject/ViewController.swift
+++ HelloWorldProject/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        welcomeLbl.text = "Welcome"
     }
 
 
